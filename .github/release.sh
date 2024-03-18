02a0cb60fe40fe98a15a774681df23cb31623eb3
Damon X Y CHENG <damoncoo@gmail.com>
2024-03-18 22:16:51 +0800

    Rename tar.gz

--- .github/release.sh
+++ .github/release.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 FILENAMES=$(ls)
-for FILENAME in $FILENAMES
-do
-    if [[ ! ($FILENAME =~ ".exe" || $FILENAME =~ ".sh")]];then
-        gzip -S ".gz" $FILENAME
-    elif [[ $FILENAME =~ ".exe" ]];then
-        zip -m ${FILENAME%.*}.zip $FILENAME
-    else echo "skip $FILENAME"
+for FILENAME in $FILENAMES; do
+    if [[ ! ($FILENAME =~ ".exe" || $FILENAME =~ ".sh") ]]; then
+        gzip -S ".gz" "$FILENAME"
+    elif [[ $FILENAME =~ ".exe" ]]; then
+        zip -m "${FILENAME%.*}".zip "$FILENAME"
+    else
+        echo "skip $FILENAME"
     fi
 done
 
 FILENAMES=$(ls)
-for FILENAME in $FILENAMES
-do
-    if [[ $FILENAME =~ ".zip" ]];then
+for FILENAME in $FILENAMES; do
+    if [[ $FILENAME =~ ".zip" ]]; then
         echo "rename $FILENAME"
-        mv $FILENAME ${FILENAME%.*}-${VERSION}.zip
-    elif [[ $FILENAME =~ ".gz" ]];then
+        mv "$FILENAME" "${FILENAME%.*}"-"${VERSION}".zip
+    elif [[ $FILENAME =~ ".gz" ]]; then
         echo "rename $FILENAME"
-        mv $FILENAME ${FILENAME%.*}-${VERSION}.gz
+        mv "$FILENAME" "${FILENAME%.*}"-"${VERSION}".tar.gz
     else
         echo "skip $FILENAME"
     fi
